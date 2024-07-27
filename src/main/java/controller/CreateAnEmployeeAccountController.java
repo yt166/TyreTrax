1372e2edfb2b681857b9ee567ce7fdaa44ec20aa
Chirath Yahampath <141777544+ChirathYahampath@users.noreply.github.com>
2024-07-28 03:37:50 +0530

    CreateanEmployeeaccount

---
+++ src/main/java/controller/CreateAnEmployeeAccountController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class CreateAnEmployeeAccountController {
+    public void closeBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void regular(ActionEvent actionEvent) {
+    }
+
+    public void BtnBackOnAction(ActionEvent actionEvent) {
+    }
+}
