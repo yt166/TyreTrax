c1c0b5d2ef9f4a2107f3b90028ad1b829548e4d0
Yuwani Ranasinghe <yuwani@agrithmics.lk>
2024-07-28 02:30:46 +0530

    Create Stock View Page

---
+++ src/main/java/controller/ViewStockController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class ViewStockController {
+    public void btnBack_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnExite_OnAction(ActionEvent actionEvent) {
+    }
+}
