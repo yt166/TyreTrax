3d27a66a22a52aa9e853e9f79f87ecd8e6061054
Yuwani Ranasinghe <yuwani@agrithmics.lk>
2024-07-28 02:43:53 +0530

    Create Purchase Order View Page

---
+++ src/main/java/controller/PurchaseOrderController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class PurchaseOrderController {
+    public void btnBack_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnExitOnAction(ActionEvent actionEvent) {
+    }
+}
